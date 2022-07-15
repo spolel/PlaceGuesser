98a72107b7cdcaec072426b12b2083d256d66327
Lorenzo Spoleti <LorenzoSpoleti@outlook.com>
2022-07-15 04:49:15 +0200

    *cant guess without marker

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  gameStarted: boolean = true
+  gameStarted: boolean = false
   helpOpen: boolean = false
 
   constructor() { }
