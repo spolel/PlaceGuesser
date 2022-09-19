d2e1134a4f1d781f526e76c7afbc08b1cf1d48a6
Lorenzo Spoleti <LorenzoSpoleti@outlook.com>
2022-09-19 23:44:15 +0200

    *fixed production keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyDiTzjP62tJNRFMdCqbpEYeqkIehzM49lY",
+  supabaseUrl: 'https://boypgzbheclrgybvbdhq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJveXBnemJoZWNscmd5YnZiZGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI2NDkyNDAsImV4cCI6MTk3ODIyNTI0MH0.AhIQgWhsRDIsENupJKNyAw4BLeiSCPnEDM9dh6yJn5U'
 };
