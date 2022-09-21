7cfde19bd6d20eb3b7678bd1e37065774c9f4b58
Lorenzo Spoleti <LorenzoSpoleti@outlook.com>
2022-09-21 12:53:43 +0200

    *updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyDiTzjP62tJNRFMdCqbpEYeqkIehzM49lY",
-  supabaseUrl: 'https://boypgzbheclrgybvbdhq.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJveXBnemJoZWNscmd5YnZiZGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI2NDkyNDAsImV4cCI6MTk3ODIyNTI0MH0.AhIQgWhsRDIsENupJKNyAw4BLeiSCPnEDM9dh6yJn5U'
+  supabaseUrl: process.env.SUPABASE_URL,
+  supabaseKey: process.env.SUPABASE_KEY
 };
